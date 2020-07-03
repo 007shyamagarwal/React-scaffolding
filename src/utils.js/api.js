d32a5a8c891d5c39314998d4f448ff8ec9fa36eb
shyam <007shyamagarwal@gmail.com>
2020-07-03 16:23:25 +0400

    updates

--- src/utils.js/api.js
+++ src/utils.js/api.js
@@ -1,8 +1,2 @@
 export const BASE_URL = process.env.REACT_APP_API_URL;
-export const ENDPOINTS = {
-  Login: '/auth/signin',
-  Register: '/auth/register',
-  Signin: '/auth/signin',
-  User: '/auth/user',
-  Logout: '/auth/logout'
-};
+export const ENDPOINTS = {};
